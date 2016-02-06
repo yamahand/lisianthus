ec768e8d0957bc81acd5c1b4abf28d7ad7746bfe
yamahand <a18293372@gmail.com>
2016-02-07 03:26:50 +0900

    delete premake

--- example/main.cpp
+++ example/main.cpp
@@ -2,15 +2,24 @@
 
 #include<test.h>
 
+#include<cstdint>
+
+constexpr size_t size = sizeof(std::int_fast32_t);
+constexpr std::uint32_t size32 = sizeof(std::int_fast32_t);
+
 int main(int argc, char* argv[]) {
 
     OutPutString("hello world!");
 
     OutPut(100);
 
-    OutPutString("hello world!");
+    OutPut("hello world!");
+
+    printf("%d, %d", size, size32);
 
     getchar();
+
+    std::int_fast16_t fast16;
 
     return 0;
 }
